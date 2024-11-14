bf6eeee3ed365e7c6e7c2b28dbebe01aa885aa70
RizharAfif <71879868+RizharAfif@users.noreply.github.com>
2024-11-14 19:00:13 +0700

    Add files via upload

---
+++ routes/CategoryRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getCategories, getCategoriesById, saveCategory, updateCategory, deleteCategory } from "../controller/CategoryController.js";
+
+const router = express.Router();
+
+router.get("/categories", getCategories);
+router.get("/categories/:id", getCategoriesById);
+router.post("/categories", saveCategory);
+router.patch("/categories/:id", updateCategory);
+router.delete("/categories/:id", deleteCategory);
+
+export default router;
